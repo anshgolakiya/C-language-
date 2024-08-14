c286123a610f865b03c1938c59297c3047e5327a
anshgolakiya <anshgolakiya9909@gmail.com>
2024-08-14 14:17:16 +0530

    Add files via upload

---
+++ condition/radious.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+void main()
+{
+    float radius ;
+
+    printf("enter a radius : ");
+    scanf("%f",&radius);
+
+    if(radius>0)
+    {
+        printf("%f",3.14*radius*radius);
+    }
+    else
+    {
+      printf("given radius is not valid.");
+    }
+    radius >0 ? 
+     printf("%f",3.14*radius*radius) :
+     printf("given radius is not valid.");
+}
