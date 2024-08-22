c667c008a4acf27eee8a439a62c39d3d9fe0a868
anshgolakiya <anshgolakiya9909@gmail.com>
2024-08-22 08:08:00 +0530

    Add files via upload

---
+++ loop/while loop/reversenumber.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+    int num, reversedNum = 0, remainder;
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+    while (num != 0) {
+        remainder = num % 10;       
+        reversedNum = reversedNum * 10 + remainder;
+        num /= 10;
+        printf("Reversed Number: %d\n", reversedNum);
+    }
+    printf("Reversed Number: %d\n", reversedNum);
+
+    return 0;
+}
