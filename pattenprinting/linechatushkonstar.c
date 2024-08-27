f6fc9bd4f73c945b329268f4bddcfcaafb1df1ea
anshgolakiya <anshgolakiya9909@gmail.com>
2024-08-27 12:29:09 +0530

    Add files via upload

---
+++ pattenprinting/linechatushkonstar.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+int main()
+{
+    int num;
+    printf("enter a number : ");
+    scanf("%d", &num);
+    for (int i = 1; i <= num; i++)
+    {
+        for (int j = i; j < num; j++)
+        {
+            printf(" ");
+        }
+        for (int j = 1; j <= (2 * i - 1); j++)
+        {
+             if (j == 1 || j == (2 * i - 1))
+             {
+                printf("*");
+             }
+             else
+             {
+                printf(" ");
+             }
+            
+        }
+        printf("\n");
+    }
+    for (int i = num; i >= 1; i--)
+    {
+        for (int j = i; j < num; j++)
+        {
+            printf(" ");
+        }
+        for (int j = 1; j <= (2 * i - 1); j++)
+        {
+             if (j == 1 || j == (2 * i - 1))
+             {
+                 printf("*");
+             }
+             else
+             {
+                 printf(" ");
+             }
+            
+        }
+        printf("\n");
+    }
+
+        return 0;
+}
