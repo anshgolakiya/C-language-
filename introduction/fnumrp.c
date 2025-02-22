01e05a091fd7a13cdbbc031fd09b0667ab548969
anshgolakiya <anshgolakiya9909@gmail.com>
2025-02-22 22:35:39 +0530

    Delete introduction directory

--- introduction/fnumrp.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main()
-{
-    float amount ;
-    printf("enter a amount : ");
-    scanf("%f",&amount);
-    int paisa = (amount - (int)amount)*100 ;
-    printf("%d rupees %d paisa",(int)amount,paisa);
-}
--- introduction/sizeof.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main()
-{
-    int num ;
-    printf("enter a number : ");
-    scanf("%d",&num);
-    printf("given number = %d if oqupite = %lu bytes",num,sizeof(num));
-    return 0 ;
-}
